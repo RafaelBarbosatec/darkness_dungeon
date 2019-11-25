9146c8163be78503cbc06c9bd28de9fadb82de67
Rafael <rafaelbarbosatec@gmail.com>
2019-11-25 11:25:15 -0300

    atualiza sprite boss

--- lib/enemies/BossEnemy.dart
+++ lib/enemies/BossEnemy.dart
@@ -10,10 +10,10 @@ class BossEnemy extends Enemy{
   static const double DAMAGE = 5;
 
   BossEnemy():super(
-      FlameAnimation.Animation.sequenced("boss_idle.png", 4, textureWidth: 32, textureHeight: 36),
+      FlameAnimation.Animation.sequenced("boss_idle.png", 4, textureWidth: 32, textureHeight: 32),
       animationDie: FlameAnimation.Animation.sequenced("enemy_explosin.png", 5, textureWidth: 16, textureHeight: 16),
       width:32,
-      height:36,
+      height:32,
       life: 300,
       intervalAtack: 500,
       speed: 1.1,
