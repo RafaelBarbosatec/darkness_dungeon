6bdd96c5d90c35b2f0084de15a422f7d6cf98512
Rafael <rafaelbarbosatec@gmail.com>
2020-03-30 01:41:31 -0300

    door

--- lib/decoration/door.dart
+++ lib/decoration/door.dart
@@ -1,5 +1,7 @@
 import 'package:bonfire/bonfire.dart';
 import 'package:darkness_dungeon/player/knight.dart';
+import 'package:darkness_dungeon/util/conversation.dart';
+import 'package:darkness_dungeon/util/talk.dart';
 import 'package:flame/animation.dart' as FlameAnimation;
 
 class Door extends GameDecoration {
@@ -39,10 +41,33 @@ class Door extends GameDecoration {
             Future.delayed(Duration(milliseconds: 200), () {
               remove();
             });
+          } else {
+            _showIntroduction();
           }
         }
       },
       visionCells: 1,
+    );
+  }
+
+  void _showIntroduction() {
+    Conversation.show(
+      gameRef.context,
+      [
+        Talk(
+          'Acho que preciso de uma chave para passar por aqui!',
+          Flame.util.animationAsWidget(
+            Position(80, 100),
+            FlameAnimation.Animation.sequenced(
+              "player/knight_idle.png",
+              4,
+              textureWidth: 16,
+              textureHeight: 22,
+            ),
+          ),
+          personDirection: PersonDirection.LEFT,
+        )
+      ],
     );
   }
 }
