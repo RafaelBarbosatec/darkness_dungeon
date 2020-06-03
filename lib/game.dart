3fd5e96d68c5587e7f27788ccc080f1d8c399c12
Rafael  Barbosa - Sciensa <rafael.sciensa@MacBook-Pro-de-adminisrador.local>
2020-06-03 15:16:34 -0300

    ajustes colisao

--- lib/game.dart
+++ lib/game.dart
@@ -75,8 +75,6 @@ class _GameState extends State<Game> implements GameListener {
       decorations: DungeonMap.decorations(),
       enemies: DungeonMap.enemies(),
       lightingColorGame: Colors.black.withOpacity(0.7),
-      showCollisionArea: true,
-      constructionMode: true,
     );
   }
 
