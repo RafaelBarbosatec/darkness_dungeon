cd00fb2b4c19006f87e6da57f3b4ddf119448f79
Rafael Barbosa <rafaelbarbosatec@gmail.com>
2021-05-26 12:42:04 -0300

    atualiza bonfire

--- lib/game.dart
+++ lib/game.dart
@@ -105,28 +105,22 @@ class _GameState extends State<Game>
         map: TiledWorldMap(
           'tiled/map.json',
           forceTileSize: Size(tileSize, tileSize),
-        )
-          ..registerObject('door',
-              (x, y, width, height) => Door(Vector2(x, y), width, height))
-          ..registerObject(
-              'torch', (x, y, width, height) => Torch(Vector2(x, y)))
-          ..registerObject(
-              'potion', (x, y, width, height) => PotionLife(Vector2(x, y), 30))
-          ..registerObject(
-              'wizard', (x, y, width, height) => WizardNPC(Vector2(x, y)))
-          ..registerObject(
-              'spikes', (x, y, width, height) => Spikes(Vector2(x, y)))
-          ..registerObject(
-              'key', (x, y, width, height) => DoorKey(Vector2(x, y)))
-          ..registerObject('kid', (x, y, width, height) => Kid(Vector2(x, y)))
-          ..registerObject('boss', (x, y, width, height) => Boss(Vector2(x, y)))
-          ..registerObject(
-              'goblin', (x, y, width, height) => Goblin(Vector2(x, y)))
-          ..registerObject('imp', (x, y, width, height) => Imp(Vector2(x, y)))
-          ..registerObject(
-              'mini_boss', (x, y, width, height) => MiniBoss(Vector2(x, y)))
-          ..registerObject('torch_empty',
-              (x, y, width, height) => Torch(Vector2(x, y), empty: true)),
+          objectsBuilder: {
+            'door': (x, y, width, height) => Door(Vector2(x, y), width, height),
+            'torch': (x, y, width, height) => Torch(Vector2(x, y)),
+            'potion': (x, y, width, height) => PotionLife(Vector2(x, y), 30),
+            'wizard': (x, y, width, height) => WizardNPC(Vector2(x, y)),
+            'spikes': (x, y, width, height) => Spikes(Vector2(x, y)),
+            'key': (x, y, width, height) => DoorKey(Vector2(x, y)),
+            'kid': (x, y, width, height) => Kid(Vector2(x, y)),
+            'boss': (x, y, width, height) => Boss(Vector2(x, y)),
+            'goblin': (x, y, width, height) => Goblin(Vector2(x, y)),
+            'imp': (x, y, width, height) => Imp(Vector2(x, y)),
+            'mini_boss': (x, y, width, height) => MiniBoss(Vector2(x, y)),
+            'torch_empty': (x, y, width, height) =>
+                Torch(Vector2(x, y), empty: true),
+          },
+        ),
         interface: KnightInterface(),
         lightingColorGame: Colors.black.withOpacity(0.6),
         background: BackgroundColorGame(Colors.grey[900]),
